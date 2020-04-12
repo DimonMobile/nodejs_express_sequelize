5075b132bd409e6d37034b5b460ae5c38780a120
Dimon Mobile <mobik.dimka@gmail.com>
2020-04-12 14:29:58 +0300

    Profile page

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,10 @@
-exports.getUserPage = function(req, res, next) {
-    res.render('users', {Title: 'User page', req: req, res: res});
+const User = require('../models/user');
+
+exports.getProfilePage = async function(req, res, next) {
+    let user = await User.findOne({where: {id: req.query.id}});
+    let isOwner = false;
+    if (user && req.session && user.dataValues.id === req.session.userId)
+        isOwner = true;
+
+    res.render('users/profile', {Title: 'User page', req: req, res: res, user: user ? user.dataValues : null, isOwner: isOwner });
 }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/users')
 
-router.get('/', (req, res, next) => controller.getUserPage(req, res, next));
+router.get('/profile', (req, res, next) => controller.getProfilePage(req, res, next));
 
 exports.router = router;
