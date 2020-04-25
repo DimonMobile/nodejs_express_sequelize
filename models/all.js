5c30131d6600319048382fc9620e92626b585914
D.Mobile <mobik.dimka@gmail.com>
2020-04-25 11:42:26 +0300

    Publication model added

--- models/all.js
+++ models/all.js
@@ -1,11 +1,17 @@
 const PublicationDraft = require('./publication_draft');
 const User = require('./user');
+const Publication = require('./publication');
 
 // relations
 User.hasMany(PublicationDraft, {
     foreignKey: 'userId'
 });
 PublicationDraft.belongsTo(User);
+
+User.hasMany(Publication, {
+    foreignKey: 'userId'
+});
+Publication.belongsTo(User);
 
 exports.PublicationDraft = PublicationDraft;
 exports.User = User;
