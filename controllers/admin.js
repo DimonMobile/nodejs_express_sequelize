e6a67e8805f9e1cc5f4c4b6e47fed9f87b01e2cf
D.Mobile <mobik.dimka@gmail.com>
2020-04-28 17:31:21 +0300

    Admin panel publications pagination implemented

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,6 @@
+const Models = require('../models/all');
+const Publication = Models.Publication;
+
 exports.getPanel = async function (req, res, next) {
     // TODO: check admin permissions
     res.render('admin/index', {title: 'Admin panel', req: req, res:res});
@@ -5,8 +8,38 @@ exports.getPanel = async function (req, res, next) {
 
 exports.getPublicationsPage = async function (req, res, next) {
     // TODO: check admin persmissions
-    // TODO: implement
-    res.render('admin/publications', {title: 'Publications administration', req: req, res: res});
+
+    let whereStrategy = {};
+    if (req.query.filter) {
+        if (req.query.filter === 'pending') {
+            whereStrategy = {reviewed: false, published: false};
+        } else if (req.query.filter === 'published') {
+            whereStrategy = {published: true, reviewed: true};
+        }
+    }
+
+    let offset = 0;
+    if (req.query.offset) {
+        offset = parseInt(req.query.offset);
+    }
+
+    let publications = await Publication.findAll({
+        where: whereStrategy,
+        order: [
+            ['updatedAt', 'DESC']
+        ],
+        limit: 5,
+        offset: offset
+    });
+    let pubs = [];
+    for (let publication of publications) {
+        pubs.push({
+            id: publication.dataValues.id,
+            title: publication.dataValues.name,
+            content: publication.dataValues.content,
+        });
+    }
+    res.render('admin/publications', {title: 'Publications administration', req: req, res: res, pubs: pubs});
 }
 
 exports.getUsersPage = async function (req, res, next) {
