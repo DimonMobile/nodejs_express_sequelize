8d864eb3d4103c845f3354558188ad74269c8e76
Dimon Mobile <mobik.dimka@gmail.com>
2020-05-05 20:38:50 +0300

    Comment model update

--- models/all.js
+++ models/all.js
@@ -1,18 +1,34 @@
 const PublicationDraft = require('./publication_draft');
 const User = require('./user');
 const Publication = require('./publication');
+const Comment = require('./comment')
 
-// relations
+// Drafts
 User.hasMany(PublicationDraft, {
     foreignKey: 'userId'
 });
 PublicationDraft.belongsTo(User);
 
+// Publications
 User.hasMany(Publication, {
     foreignKey: 'userId'
 });
 Publication.belongsTo(User);
 
+// Comments
+User.hasMany(Comment, {
+    foreignKey: 'userId'
+});
+
+Publication.hasMany(Comment, {
+    foreignKey: 'publicationId'
+});
+
+Comment.belongsTo(User);
+Comment.belongsTo(Comment);
+
+// Exports
 exports.PublicationDraft = PublicationDraft;
 exports.User = User;
 exports.Publication = Publication;
+exports.Comment = Comment;
